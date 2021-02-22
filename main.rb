525a9849e5e3bc48dcde8cf1d7c84625954a675c
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-22 13:35:53 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-#creating a class
+# Ceating a class (Car) assigning attributes: name, make, model, year
 class Car 
   attr_accessor :name, :make, :model, :year
   def initialize(name, make, model, year)
@@ -9,17 +9,16 @@ class Car
     
   end
 
-#creating object(s) on a single line
+# Creating objects on a single line and assigning data to the appropriate attributes
 car1 = Car.new("Gemma", "Porsche", "944 S2", "1991")
 car2 = Car.new("Alex", "Volkswagen", "MK1 Golf", "1983")
 
-# accessing objects using interpolation in a sentence
+# Accessing objects using interpolation in a sentence
 puts ("--- #{car1.name}'s Car---")
 puts "My car is a #{car1.make} #{car1.model}, it was also manufactured in the same year I was born, which is #{car1.year}."
 
-#accessing information from Car 2 in a print statement as above
+# Accessing information from Car 2 in a print statement as above
 puts ("--- #{car2.name}'s Car---")
 puts "My Fiance's car is a #{car2.make} #{car2.model} and is a year younger than him, as it was manufactured in #{car2.year}."
 
 end
-
